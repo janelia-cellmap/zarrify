608a0a0e113995537826752b0888fec60ff263e2
Yurii Zubov <zubov452@gmail.com>
2025-07-23 16:01:28 -0400

    :bug: resolve dask serialization issue for the lambda function that contains reference to self instance

--- src/zarrify/formats/mrc.py
+++ src/zarrify/formats/mrc.py
@@ -10,6 +10,7 @@ from zarrify.utils.volume import Volume
 from abc import ABCMeta
 from numcodecs import Zstd
 import logging
+import copy
 
 class Mrc3D(Volume):
 
@@ -30,20 +31,9 @@ class Mrc3D(Volume):
 
         self.memmap = mrcfile.mmap(self.src_path, mode="r")
         self.ndim = self.memmap.data.ndim
-        self.shape = self.memmap.shape
+        self.shape = self.memmap.data.shape
         self.dtype = self.memmap.data.dtype
 
-    def save_chunk(self, z_arr: zarr.Array, chunk_slice: Tuple[slice, ...]):
-        """Copies data from a particular part of the input mrc array into a specific chunk of the output zarr array.
-
-        Args:
-            z_arr (zarr.core.Array): output zarr array object
-            chunk_slice (Tuple[slice, ...]): slice of the mrc array to copy.
-        """
-        mrc_file = mrcfile.mmap(self.src_path, mode="r")
-
-        if not (mrc_file.data[chunk_slice] == 0).all():
-            z_arr[chunk_slice] = mrc_file.data[chunk_slice]
 
     def write_to_zarr(
         self,
@@ -62,8 +52,9 @@ class Mrc3D(Volume):
         logging.basicConfig(level=logging.INFO, 
                          format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-        
+        src_path = copy.copy(self.src_path)
         z_arr = self.get_output_array(dest, zarr_chunks, comp)
+        
         out_slices = slices_from_chunks(
             normalize_chunks(z_arr.chunks, shape=z_arr.shape)
         )
@@ -73,10 +64,22 @@ class Mrc3D(Volume):
 
             logging.info(f"{idx + 1} / {len(out_slices_partitioned)}")
             start = time.time()
-            fut = client.map(lambda v: self.save_chunk(z_arr, v), part)
+            fut = client.map(lambda v: save_chunk(src_path, z_arr, v), part)
             logging.info(
                 f"Submitted {len(part)} tasks to the scheduler in {time.time()- start}s"
             )
             # wait for all the futures to complete
             result = wait(fut)
             logging.info(f"Completed {len(part)} tasks in {time.time() - start}s")
+
+def save_chunk(src_path, z_arr: zarr.Array, chunk_slice: Tuple[slice, ...]):
+    """Copies data from a particular part of the input mrc array into a specific chunk of the output zarr array.
+
+    Args:
+        z_arr (zarr.core.Array): output zarr array object
+        chunk_slice (Tuple[slice, ...]): slice of the mrc array to copy.
+    """
+    mrc_file = mrcfile.mmap(src_path, mode="r")
+
+    if not (mrc_file.data[chunk_slice] == 0).all():
+        z_arr[chunk_slice] = mrc_file.data[chunk_slice]
