05a7fe759c96548dd76b906c0985c7e97a368082
Yurii Zubov <zubov452@gmail.com>
2025-09-05 11:17:23 -0400

    Change input parameter from dest path to Zarr group

--- src/zarrify/utils/zarr_utils.py
+++ src/zarrify/utils/zarr_utils.py
@@ -2,11 +2,11 @@ import zarr
 from abc import ABCMeta
 
 
-def create_output_array(dest: str, shape: tuple, dtype, chunks: list[int], comp: ABCMeta) -> zarr.Array:
+def create_output_array(z_root: zarr.Group, shape: tuple, dtype, chunks: list[int], comp: ABCMeta) -> zarr.Array:
     """Create a zarr array at the specified destination with given parameters.
     
     Args:
-        dest (str): Output zarr group location
+        z_root (zarr.Group): Output zarr group
         shape (tuple): Shape of the array
         dtype: Data type of the array
         chunks (list[int]): Chunk sizes for the array
@@ -15,8 +15,7 @@ def create_output_array(dest: str, shape: tuple, dtype, chunks: list[int], comp:
     Returns:
         zarr.Array: The created zarr array
     """
-    z_store = zarr.NestedDirectoryStore(dest)
-    z_root = zarr.open(store=z_store, mode="a")
+
     
     return z_root.require_dataset(
         name="s0",
